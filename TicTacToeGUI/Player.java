bcaa2ce604f4c372f04957502f2e750b3eecd6e8
Esteban Acosta <estebanacosta19@gmail.com>
2021-06-15 23:05:26 -0400

    Added a players class

---
+++ TicTacToeGUI/Player.java
@@ -0,0 +1,35 @@
+package TicTacToeGUI;
+
+public class Player
+{
+    private String name;
+
+    private int playerId;
+
+
+    public Player(int id)
+    {
+        setPlayerId(id);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getPlayerId()
+    {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId)
+    {
+        this.playerId = playerId;
+    }
+
+}
