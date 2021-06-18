ff1cb9f9b3f0cb3ce3c577274e0478532e8726ed
Esteban Acosta <estebanacosta19@gmail.com>
2021-06-18 16:04:53 -0400

    Added the ability to decide which letter each player gets, and ask how many rounds each player wnats to play

--- TicTacToeGUI/TicTacToe.java
+++ TicTacToeGUI/TicTacToe.java
@@ -20,50 +20,85 @@ public class TicTacToe
     {
         TicTacFrame tt = new TicTacFrame();
 
-        int min = 1;
-
-        int max = 100;
-
+        // loop through the players of this game
         for (int i = 0; i < 2; i++)
         {
+            // give each player an id number
             players[i] = new Player(i + 1);
 
+            // ask each player what's their name and set that name for each player
             players[i].setName(JOptionPane.showInputDialog("What's your name, Player " + (i + 1)));
 
+            // display the player's name
             JOptionPane.showMessageDialog(new JFrame(), "Player " + (i + 1) + "'s name is " + players[i].getName());
 
+            // if this is the first player
             if (i == 0)
             {
-                String number = JOptionPane.showInputDialog("Pick a number between " + min + " and " + max);
+                // Ask the user to enter a number
+                String number = JOptionPane.showInputDialog("Enter an integer");
 
+                // if the response isn't an integer
                 while (!number.matches("[0-9]+"))
                 {
-                    JOptionPane.showMessageDialog(new JFrame(), "Please enter a whole number between " + min + " and " + max);
 
-                    number = JOptionPane.showInputDialog("Pick a whole number between " + min + " and " + max);
+                    //display an error message showing that the player hasn't entered an integer
+                    JOptionPane.showMessageDialog(new JFrame(), "The response you have provided isn't an integer. Please enter a whole number");
+
+                    //try to get an input from this player again
+                    number = JOptionPane.showInputDialog("Please enter a whole number");
 
                 }
 
+                //if the number that the player provided is an even integer
                 if (Integer.parseInt(number) % 2 == 0)
                 {
-                    JOptionPane.showMessageDialog(new JFrame(), "Player " + (i + 1) + " " + players[i].getName() + " gets to place an X");
 
+                    //this player gets to use X's for this round
                     players[i].setXorO("X");
 
                 }
 
+                //if the number that the player provided is an odd integer
                 else
                 {
-                    JOptionPane.showMessageDialog(new JFrame(), "Player " + (i + 1) + " " + players[i].getName() + " gets to place an O");
-
+                    //this player gets to use O's for this round
                     players[i].setXorO("O");
 
                 }
+
+                //Display the letter the second player gets to use in this round
+                JOptionPane.showMessageDialog(new JFrame(), "Player " + (i + 1) + " " + players[i].getName() + " gets to place an " + players[i].getXorO());
 
             }
 
         }
 
+        //if the first player who got to decide which letter they wanted got an X
+        if (players[0].getXorO().equalsIgnoreCase("X"))
+        {
+            //second player by default gets an O
+            players[1].setXorO("O");
+
+        }
+
+        //if the first player who got to decide which letter they wanted got an O
+        else
+        {
+            //second player by default gets an X
+            players[1].setXorO("X");
+
+        }
+
+        JOptionPane.showMessageDialog(new JFrame(), "Player " + players[1].getPlayerId() + " " + players[1].getName() + " gets to place an " + players[1].getXorO());
+
+        int minRound = 1;
+        
+        int maxRound = 5;
+        
+        String rounds = JOptionPane.showInputDialog("Please enter a whole number for the number of rounds both players want to play? Minimum is " + minRound + " and Maximum is " + maxRound );
+
+        
         tt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         tt.setVisible(true);
@@ -92,6 +127,7 @@ class TicTacPanel extends JPanel
 
     public TicTacPanel()
     {
+        //Make the panel layout a border layout
         setLayout(new BorderLayout());
 
         JButton score = new JButton();
