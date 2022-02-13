483890f434945558e5fbe6ef930da707a939a730
Esteban Acosta <estebanacosta19@gmail.com>
2022-02-13 00:46:02 -0500

    Added a new men u bar

--- ChessGUI/chessPanel.java
+++ ChessGUI/chessPanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
@@ -23,11 +24,49 @@ public class chessPanel extends JPanel
 
         JMenuBar menuBar = new JMenuBar();
 
-        JMenuItem newGame = new JMenuItem();
+        JMenu openFile = new JMenu("Open File");
+
+        JMenuItem openFileOption = new JMenuItem("Open File");
+
+        openFile.add(openFileOption);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        JMenu saveGame = new JMenu("Save Game");
+
+        JMenuItem saveGameOption = new JMenuItem("Save Game");
+
+        saveGame.add(saveGameOption);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        JMenu newGame = new JMenu("New Game");
+
+        JMenuItem newGameOption = new JMenuItem("New Game");
+
+        newGame.add(newGameOption);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        JMenu undoMove = new JMenu("Undo Move");
+
+        JMenuItem undoMoveOption = new JMenuItem("Undo Move");
+
+        undoMove.add(undoMoveOption);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        menuBar.add(openFile);
+        
+        menuBar.add(saveGame);
 
         menuBar.add(newGame);
 
+        menuBar.add(undoMove);
+
         upperPanel.add(menuBar);
+
+        ///////////////////////////////////////////////////////////////////////////
 
         JPanel centerPanel = new JPanel();
 
